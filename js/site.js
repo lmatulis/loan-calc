f418f370b17914aa8c9473f4509725d573d02d72
lmatulis <matulisluke@gmail.com>
2024-07-19 17:09:56 -0500

    init c ommit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,16 @@
-//
-function getValues(message) {
-    alert(message);
+
+function getValues() {
+    let loanAmount = document.getElementById("loanAmount").value;
+    let termMonths = document.getElementById("termMonths").value;
+    let interestRate = document.getElementById("interestRate").value;
+
+    let monthlyPayment = document.getElementById("monthlyPayment");
+    monthlyPayment.innerHTML = calculateTotalMonthlyPayments(loanAmount,interestRate,termMonths);
 }
 
-//Get values from the screen and display
-function getValues2() {
-    let message = document.getElementById("message").value
-    let alertbox = document.getElementById("alertBox")
 
-    alertbox.innerText = message
+function calculateTotalMonthlyPayments(loanAmount, interestRate, termMonths) {
+    let x = 1+(interestRate/1200);
+    let totalMonthlyPayment = (loanAmount*(interestRate/1200))/(1-Math.pow(x,termMonths*-1));
+    return totalMonthlyPayment;
 }
