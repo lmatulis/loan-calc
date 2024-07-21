e5ef2b2b050629edd0731a8290508c6b8166847a
lmatulis <matulisluke@gmail.com>
2024-07-20 20:06:47 -0500

    got the calculations working. temporarily made them appear on screen.

--- js/site.js
+++ js/site.js
@@ -4,13 +4,49 @@ function getValues() {
     let termMonths = document.getElementById("termMonths").value;
     let interestRate = document.getElementById("interestRate").value;
 
-    let monthlyPayment = document.getElementById("monthlyPayment");
-    monthlyPayment.innerHTML = calculateTotalMonthlyPayments(loanAmount,interestRate,termMonths);
+    loanAmount = parseInt(loanAmount);
+    termMonths = parseInt(termMonths);
+    interestRate = parseInt(interestRate);
+
+    if(Number.isInteger(loanAmount) && Number.isInteger(termMonths) && Number.isInteger(interestRate)) {
+
+        let TMP = calculateTotalMonthlyPayments(loanAmount, interestRate, termMonths);
+        let totalInterest = calculateTotalInterest(loanAmount, interestRate, termMonths);
+        let totalCost = calculateTotalCost(totalInterest, loanAmount);
+
+        document.getElementById("monthlyPayment").innerHTML = `$${TMP}`;
+        document.getElementById("totalPrincipal").innerHTML = `$${loanAmount}`;
+        document.getElementById("totalInterest").innerHTML = `$${totalInterest}`;
+        document.getElementById("totalCost").innerHTML = `$${totalCost}`;
+
+    } else {
+        alert("Only numbers allowed. Please enter numbers into the fields");
+    }
 }
 
+function displayData(TMP) {
+
+}
 
 function calculateTotalMonthlyPayments(loanAmount, interestRate, termMonths) {
-    let x = 1+(interestRate/1200);
-    let totalMonthlyPayment = (loanAmount*(interestRate/1200))/(1-Math.pow(x,termMonths*-1));
-    return totalMonthlyPayment;
+    let base = 1 + (interestRate/1200);
+    let pow = -1*termMonths;
+
+    let TMP = (loanAmount) * (interestRate/1200)/(1-(Math.pow(base, pow)));
+    return TMP.toFixed(2);
+}
+
+function calculateTotalInterest(loanAmount, interestRate, termMonths) {
+
+    let base = 1 + (interestRate/1200);
+    let pow = -1*termMonths;
+
+    let totalInterest = ((loanAmount) * (interestRate/1200)/(1-(Math.pow(base, pow))))*termMonths-loanAmount;
+    return totalInterest.toFixed(2);
+
+}
+
+function calculateTotalCost(totalInterest, loanAmount) {
+    let totalCost = totalInterest + loanAmount;
+    return totalCost;
 }
